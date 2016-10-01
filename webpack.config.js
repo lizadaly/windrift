c14a8de798c4cb96fbbaac5ba95ab57c694c1269
Liza Daly <lizadaly@gmail.com>
2016-10-01 14:41:18 -0400

    Refactor name of entry point

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,42 @@ var PROD = (process.env.NODE_ENV === 'production')
 module.exports = {
 
     context: __dirname + "/src",
-    entry: "./script",
+    entry: "./app",
     output: {
         path: __dirname + "/build",
-        filename: PROD ? 'bundle.min.js' : 'bundle.js'
+        filename: PROD
+            ? 'bundle.min.js'
+            : 'bundle.js'
     },
     module: {
         loaders: [
             {
                 test: /.js/,
-                loaders: ['babel?cacheDirectory'],
-            },
-            {
+                loaders: ['babel?cacheDirectory']
+            }, {
                 test: /\.json$/,
                 loader: 'json'
             }
         ]
     },
-    plugins: PROD ? [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-              warnings: false,
-              screw_ie8: true
-            },
-            comments: false
-        }),
-	     new webpack.DefinePlugin({
-	          "process.env": {
-		            NODE_ENV: JSON.stringify("production")
-	             }
-       })
-    ] : [
-	new webpack.DefinePlugin({
-	    "process.env": {
-		NODE_ENV: JSON.stringify("develop")
-	    }
-	})
-    ]
+    plugins: PROD
+        ? [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    screw_ie8: true
+                },
+                comments: false
+            }),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify("production")
+                }
+            })
+        ]
+        : [new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify("develop")
+                }
+            })]
 };
