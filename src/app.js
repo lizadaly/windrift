d90b9b46cbf66c40b17855d4350d3f1faa0c4128
Liza Daly <lizadaly@gmail.com>
2017-07-26 18:22:47 -0400

    [#17] Reconstitute state on load even if there's no popstate event

--- src/app.js
+++ src/app.js
@@ -53,6 +53,14 @@ const mapStateToProps = (state) => {
   }
 }
 
+/* Jump to a point in time from the browser history, either onload or after a popstate event */
+const jumpFromHistory = (store) => {
+  if (history.state.hasOwnProperty(store.getState().config.identifier)) {
+    let timeOffset = history.state[store.getState().config.identifier] - store.getState().counter.present
+    store.dispatch(ActionCreators.jump(timeOffset))
+  }
+}
+
 export const Game = connect(
   mapStateToProps, {
     showNextSection: showNextSection
@@ -61,16 +69,19 @@ export const Game = connect(
 
 export const startGame = (game) => {
   var store = createStore(gameApp, {config: game.props.config}, autoRehydrate())
-  var persister = persistStore(store, {keyPrefix: game.props.config.identifier})
+  // Set up the localStorage persistence
+  persistStore(store, {keyPrefix: game.props.config.identifier})
 
+  // Jump to the current point in history after hydration if there's any state at all
+  if (history.state) {
+    jumpFromHistory(store)
+  }
+  
   if (game.props.config.enableUndo) {
+    // If we received a browser forward/back, jump to the relevant point in history
     window.addEventListener("popstate", function(e) {
-      if (history.state.hasOwnProperty(store.getState().config.identifier)) {
-        let timeOffset = history.state[store.getState().config.identifier] - store.getState().counter.present
-        store.dispatch(ActionCreators.jump(timeOffset))
-      }
+      jumpFromHistory(store)
     })
   }
   ReactDOM.render(<Provider store={store}>{game}</Provider>, document.getElementById('article'))
-
 }
