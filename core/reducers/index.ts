86d1dbbd37589bce87cf622eb1ded5e934d6cf6a
Liza Daly <lizadaly@gmail.com>
2020-11-11 16:18:10 -0500

    add config

--- core/reducers/index.ts
+++ core/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import inventory from './inventory'
 import expansions from './expansions'
 import counter from './counter'
+import config from "./config"
 
 const rootReducer = combineReducers({
     inventory,
     counter,
     expansions,
+    config,
 })
 export type RootState = ReturnType<typeof rootReducer>
 export default rootReducer
