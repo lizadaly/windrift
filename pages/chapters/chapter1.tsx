b169b0691eda9538adbd1588ef9002ef0485e610
Liza Daly <lizadaly@gmail.com>
2020-11-14 14:08:24 -0500

    Add a second section

--- pages/chapters/chapter1.tsx
+++ pages/chapters/chapter1.tsx
@@ -2,8 +2,16 @@ import * as React from "react"
 import { List } from '../../core/components'
 
 export default (): JSX.Element => (
-    <section>
-        <h1>Chapter 1</h1>
-        <List expansions={["dog", "cat"]} tag="foo" />
-    </section>
+    <div>
+        <section>
+            <h1>Chapter 1</h1>
+            <List expansions={["dog", "cat"]} tag="foo" />
+        </section>
+        <section>
+            <h2>Section 2</h2>
+            <p>
+                hello world.
+        </p>
+        </section>
+    </div>
 )
