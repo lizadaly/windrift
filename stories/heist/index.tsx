066a81b2bc7315979f4242cdb97426166ce0839d
Liza Daly <lizadaly@gmail.com>
2020-11-23 12:56:32 -0500

    Add Heist skeleton

---
+++ stories/heist/index.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import * as React from "react"
+import { RootState } from '../../core/reducers'
+import { useSelector } from 'react-redux'
+
+import { resetGame } from '../../core/util'
+
+import styles from './Index.module.scss'
+
+const Index: React.FC = ({ children }): JSX.Element => {
+    const config = useSelector((state: RootState) => state.config)
+
+    return (
+        <>
+            <Head>
+                <title>{config.title}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=EB+Garamond&family=Elsie&display=swap');
+                </style>
+            </Head>
+
+            <header className={styles.header}>
+                <nav>
+                    <h1>
+                        {config.title}
+                    </h1>
+                    <div className={styles.controls}>
+                        <button onClick={resetGame}>Reset</button>
+                    </div>
+                </nav>
+            </header>
+            <main className={styles.main}>
+                <nav className={styles.left}>
+
+                </nav>
+                <article className={styles.article}>
+                    {children}
+                </article>
+                <nav className={styles.right}>
+
+                </nav>
+            </main>
+        </>
+    )
+}
+
+export default Index
