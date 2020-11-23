fb6e9591f38485790b10ce15d92b4b8ba4d1b626
Liza Daly <lizadaly@gmail.com>
2020-11-22 21:44:49 -0500

    CSS grid defaults

--- stories/demo/index.tsx
+++ stories/demo/index.tsx
@@ -3,6 +3,8 @@ import * as React from "react"
 import { RootState } from '../../core/reducers'
 import { useSelector } from 'react-redux'
 
+import { resetGame } from '../../core/util'
+
 import styles from './Index.module.scss'
 
 const Index: React.FC = ({ children }): JSX.Element => {
@@ -14,9 +16,28 @@ const Index: React.FC = ({ children }): JSX.Element => {
                 <title>{config.title}</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <div className={styles.container}>
-                {children}
-            </div>
+
+            <header className={styles.header}>
+                <nav>
+                    <h1>
+                        {config.title}
+                    </h1>
+                    <div className={styles.controls}>
+                        <button onClick={resetGame}>Reset</button>
+                    </div>
+                </nav>
+            </header>
+            <main className={styles.main}>
+                <nav className={styles.left}>
+
+                </nav>
+                <article className={styles.article}>
+                    {children}
+                </article>
+                <nav className={styles.right}>
+
+                </nav>
+            </main>
         </>
     )
 }
