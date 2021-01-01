7e47213554a426b893b4cf1259cca2fbd456047c
Liza Daly <lizadaly@gmail.com>
2021-01-01 10:17:26 -0500

    Add reset/init

--- core/reducers/index.ts
+++ core/reducers/index.ts
@@ -6,8 +6,9 @@ import config from './config'
 import toc from './toc'
 import multiplayer from './multiplayer'
 import log from './log'
+import { INIT_STATE } from 'core/actions/init'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
     inventory,
     counter,
     choices,
@@ -16,5 +17,11 @@ const rootReducer = combineReducers({
     multiplayer,
     log
 })
+const rootReducer = (state: any, action: any) => {
+    if (action.type == INIT_STATE) {
+        state = undefined
+    }
+    return appReducer(state, action)
+}
 export type RootState = ReturnType<typeof rootReducer>
 export default rootReducer
