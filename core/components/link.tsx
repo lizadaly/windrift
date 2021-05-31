bbcc9538fdc417e9ee580a8a9ec026096502303d
Liza Daly <lizadaly@gmail.com>
2021-05-31 10:42:43 -0400

    Add data attribute for tag name used in testing

--- core/components/link.tsx
+++ core/components/link.tsx
@@ -3,16 +3,19 @@ is supplied, then the text is displayed as static HTML.
 
 This typically occurs for the last item in a Choice list. */
 
+import { Tag } from 'core/types'
+
 interface LinkProps {
     text: string
     index?: number
     handler: any
+    tag?: Tag
 }
-const Link = ({ text, index, handler }: LinkProps): JSX.Element => {
+const Link = ({ text, index, handler, tag }: LinkProps): JSX.Element => {
     if (handler) {
         return (
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-            <a href="#" onClick={() => handler(event, index)}>
+            <a href="#" data-tag={tag} onClick={() => handler(event, index)}>
                 {text}
             </a>
         )
