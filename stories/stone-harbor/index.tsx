9dc7a77500c5e66cf015c98b208455f9d974b064
Liza Daly <lizadaly@gmail.com>
2021-10-05 12:35:05 -0400

    Font customizations

--- stories/stone-harbor/index.tsx
+++ stories/stone-harbor/index.tsx
@@ -6,7 +6,14 @@ import styles from 'public/stories/stone-harbor/styles/Index.module.scss'
 
 const Index: React.FC = ({ children }) => {
     return (
-        <Grid styles={styles} head={<style></style>}>
+        <Grid
+            styles={styles}
+            head={
+                <style>
+                    @import
+                    url('https://fonts.googleapis.com/css2?family=STIX+Two+Math,Raleway&display=swap');
+                </style>
+            }>
             {children}
         </Grid>
     )
