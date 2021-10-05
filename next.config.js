919acb8e09ec03f6f3fafcb0755a1f7a1e5a2bcc
Liza Daly <lizadaly@gmail.com>
2021-10-05 10:08:48 -0400

    [#42] Add Stone Harbor prologue in english

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    i18n: {
+        // Add any addition new languages for stories here
+        locales: ['en', 'pt'],
+
+        defaultLocale: 'en',
+        localeDetection: false
+    }
+}
