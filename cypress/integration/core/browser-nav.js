9d9b70a4905435ba6b70a8c50e2995f1568a86f2
Liza Daly <lizadaly@gmail.com>
2021-10-31 17:33:02 -0400

    Why is this broken

--- cypress/integration/core/browser-nav.js
+++ cypress/integration/core/browser-nav.js
@@ -2,10 +2,10 @@ describe('Full test of browser forward/back features', () => {
     it('Checks back', () => {
         cy.visit('/demo')
         cy.get('a').contains('about choices').click()
-        cy.get('a').contains('ripe banana')
+        cy.get('a').contains('ripe banana').should('exist')
         cy.get('a').contains('bulbous orange').click()
         cy.go('back')
-        cy.get('a').contains('bulbous orange')
+        cy.get('a').contains('bulbous orange').should('exist')
         // Now make a different choice
         cy.get('a').contains('ripe banana').click()
     })
