8f5928e18e00617b295902121223f3d6e0672411
Liza Daly <lizadaly@gmail.com>
2021-11-07 14:48:50 -0500

    More on deployment

--- stories/demo/chapters/deploying.tsx
+++ stories/demo/chapters/deploying.tsx
@@ -10,10 +10,9 @@ export const Page: PageType = () => {
                 <p>
                     NextJS, and therefore Windrift, have two mechanisms of deploying your
                     application: a purely "static" generation mechanism, and one that requires use
-                    of a backend server. Most Windrift stories are static only, meaning they have no
-                    backend needed and can be deployed on any site capable of hosting HTML files.
-                    This document will focus on static deployment and hosting, which is the most
-                    flexible.
+                    of a backend server. Most Windrift stories are static only, meaning they need no
+                    backend and can be deployed on any most site capable of hosting HTML files. This
+                    document will focus on static deployment and hosting.
                 </p>
                 <h3>Generating a static build</h3>
                 <p>
@@ -70,17 +69,51 @@ _next/           global.scss      minimal/         stone-harbor-pt/ styles/
 `}
                 </SyntaxHighlighter>
                 <p>
-                    Because of browser security restrictions you won't be able to just open those
-                    HTML files from your local file system, but dropping the entire <code>out</code>{' '}
-                    directory in Amazon S3 or any HTML-based hosting environment will work.
+                    Note that each story is containined in a subdirectory containing an{' '}
+                    <code>index.html</code> file. The easiest way to deploy a Windrift instance is
+                    to copy the entire <code>out</code> folder to a bucket in Amazon S3, to use
+                    Github Pages, or copy to any HTML-based hosting environment.
+                </p>
+                <p>
+                    Don't just copy the single HTML file for your story—the actual source code is
+                    contained in the <code>_next/</code> folder at the top of the tree. Any images
+                    or CSS will also be at the root of the <code>out</code> folder. If you need to
+                    output the JavaScript at a different location, you can modify{' '}
+                    <code>next.config.js</code> at the root of the Windrift installation to use a
+                    different <code>assetPath</code>, but this will not affect images or other
+                    static assets. (Unfortunately the NextJS options for fine-grained control over
+                    exported static asset paths are{' '}
+                    <a href="https://github.com/vercel/next.js/issues/2581">somewhat limited</a>—you
+                    may be better off doing a post-hoc transformation of the generated HTML.)
+                </p>
+                <p>
+                    More information on the options and limitations of static output can be found in
+                    the{' '}
+                    <a href="https://nextjs.org/docs/advanced-features/static-html-export">
+                        NextJS static HTML export
+                    </a>{' '}
+                    page.
                 </p>
                 <h3>Hosting with itch.io</h3>
                 <p>
                     At the moment the file structure output by NextJS's static export is
-                    incompatible with itch.io's requirements for HTML games. A workaround commonly
-                    suggested by other React game developers is to host your exported story on
-                    something like Github Pages, then on itch.io upload an HTML file that contains
-                    just an iframe to your hosted story on Github.
+                    incompatible with itch.io's{' '}
+                    <a href="https://itch.io/docs/creators/html5">requirements for HTML games</a>. A
+                    workaround commonly suggested by other React game developers is to host your
+                    exported story on something like Github Pages, then on itch.io upload an HTML
+                    file that contains just an iframe to your hosted story on Github.
+                </p>
+                <h3>Hosting with Vercel</h3>
+                <p>
+                    By far the most straightforward hosting mechanism for any NextJS application is
+                    to use the hosting site <a href="https://vercel.com">Vercel</a>, which is run by
+                    the same company that produces NextJS itself. (This manual is itself hosted on
+                    Vercel at <a href="https://windrift.app/demo">windrift.app</a>.) Hosting with
+                    Vercel is free, and provides many other benefits including use of NextJS's
+                    server-side features. However, Vercel may not exist forever, so it is always
+                    desirable to use purely static exports for longevity and inclusion on sites like
+                    the <a href="https://www.ifarchive.org/">IF Archive</a>, assuming the story
+                    itself allows for this option.
                 </p>
             </Section>
         </Chapter>
