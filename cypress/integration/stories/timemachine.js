0c29f755b4f69c7671d9cd461bd95c111988384b
Liza Daly <lizadaly@gmail.com>
2021-11-14 15:46:53 -0500

    Fix test

--- cypress/integration/stories/timemachine.js
+++ cypress/integration/stories/timemachine.js
@@ -2,9 +2,10 @@ describe('Full test of the Time Machine story', () => {
     it('Checks that the story works', () => {
         cy.visit('/playground')
         cy.get('a:contains("been...")').click()
-        cy.get('a:contains("enticing")').click()
-        cy.get('a:contains("time machine’s lever")').click()
-        cy.get('a:contains("worn lever")').click()
+        cy.get('button:contains("enticing")').click()
+        cy.get('button:contains("lever")').click()
+        cy.get('button:contains("worn lever")').click()
+        cy.get('button:contains("wobbling lever")').click()
         cy.get('a:contains("gaping open")').click()
         cy.contains('You clamber').should('exist')
     })
