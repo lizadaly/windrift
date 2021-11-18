72d7d7e6eaa34bc0ea35bf8738a87c8bb631c256
Liza Daly <lizadaly@gmail.com>
2021-11-18 09:54:47 -0500

    Add assets

--- stories/playground/chapters/game-of-cards.tsx
+++ stories/playground/chapters/game-of-cards.tsx
@@ -23,6 +23,7 @@ interface Item {
     cls: string[]
     x: number
     y: number
+    z: number
 }
 interface SceneryProps {
     items: Item[]
@@ -31,24 +32,22 @@ interface SceneryProps {
 const Scenery = ({ items, sceneConfig = config.stiff }: SceneryProps): JSX.Element => {
     const { show } = React.useContext(SceneContext)
     const showables = show ? items : []
-    return (
-        <Transition
-            items={showables}
-            from={(item) => ({ translateX: `${item.x}px` })}
-            enter={{
-                translateX: '0px'
-            }}
-            leave={(item) => [{ translateX: `${item.x}px` }]}
-            delay={200}
-            config={sceneConfig}>
-            {(styles, item) =>
-                item && (
-                    <animated.div
-                        style={styles}
-                        className={item.cls.map((c) => `${cards[c]}`).join(' ')}></animated.div>
-                )
-            }
-        </Transition>
+    const transitions = useTransition(showables, {
+        from: ({ x, y, z }) => ({ x, y, z }),
+        enter: { x: 0, y: 0, z: 0 },
+        leave: ({ x, y, z }) => ({ x, y, z }),
+        delay: 200,
+        config: sceneConfig
+    })
+    return transitions(
+        ({ x, y, z }: Item, item) =>
+            item && (
+                <animated.div
+                    style={{
+                        transform: to([x, y, z], (x, y, z) => `translate3d(${x}px, ${y}px, ${z}px)`)
+                    }}
+                    className={item.cls.map((c) => `${cards[c]}`).join(' ')}></animated.div>
+            )
     )
 }
 interface CardProps {
@@ -62,11 +61,11 @@ const Card: React.FC<CardProps> = ({ children, className = '' }) => {
         leave: { xyz: [0, 200, 0] }
     })
     return transitions(
-        (styles, item) =>
+        ({ xyz }, item) =>
             item && (
                 <animated.div
                     style={{
-                        transform: styles.xyz.to((x, y, z) => `translate3d(${x}px, ${y}px, ${z}px)`)
+                        transform: xyz.to((x, y, z) => `translate3d(${x}px, ${y}px, ${z}px)`)
                     }}
                     className={`${cards.card} ${className}`}>
                     {children}
@@ -78,13 +77,14 @@ const Card: React.FC<CardProps> = ({ children, className = '' }) => {
 export const SceneContext = React.createContext<Partial<SceneContextProps>>({})
 interface SceneProps {
     turn: number
+    className: string
 }
-const Scene: React.FC<SceneProps> = ({ children, turn }) => {
+const Scene: React.FC<SceneProps> = ({ children, turn, className }) => {
     const item = useChapter()
 
     return (
         <SceneContext.Provider value={{ show: item.bookmark === turn }}>
-            {children}
+            <div className={`${className} ${cards.scene}`}>{children}</div>
         </SceneContext.Provider>
     )
 }
@@ -95,20 +95,29 @@ export const Page: PageType = () => {
         <div className={cards.container}>
             <ChapterContext.Provider value={{ filename }}>
                 <Stage>
-                    <Scene turn={0}>
+                    <Scene turn={0} className={cards.beach}>
                         <Card className={cards.card1}>
-                            <h2>Card 1</h2>
-                            <C options={[['Go to next scene']]} tag="scene1" persist={true} />
+                            <h2>Talk to the animals!</h2>
+                            <p>
+                                It's a beautiful day at the beach and you can talk to animals. What
+                                do they have to say?
+                            </p>
+                            {/* <C options={[['Go to next scene']]} tag="scene1" persist={true} /> */}
                         </Card>
                         <Scenery
                             items={[
-                                { cls: ['left', 'tree', 'object'], x: -200, y: 0 },
-                                { cls: ['right', 'tree', 'object'], x: 500, y: 0 }
+                                { cls: ['sand', 'bottom', 'object'], x: 0, y: 100, z: 0 },
+                                { cls: ['palm1', 'left', 'object'], x: -200, y: 0, z: 0 },
+                                { cls: ['palm1', 'right', 'object'], x: 500, y: 0, z: 0 }
                             ]}
+                        />
+                        <Scenery
+                            sceneConfig={config.molasses}
+                            items={[{ cls: ['crab1', 'object'], x: -900, y: 0, z: 0 }]}
                         />
                     </Scene>
 
-                    <Scene turn={1}>
+                    <Scene turn={1} className={cards.ocean}>
                         <Card className={cards.card2}>
                             <h2>Card 2</h2>
                             <p>Has some other text.</p>
