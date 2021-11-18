aefee3c6a8ac1157b510295d01de8fc9650d7977
Liza Daly <lizadaly@gmail.com>
2021-11-18 15:10:41 -0500

    Add multiresponse

--- core/components/response.tsx
+++ core/components/response.tsx
@@ -1,41 +1,32 @@
 import minimatch from 'minimatch'
 
-import { useSelector } from 'react-redux'
-import { RootState } from 'core/types'
+import { Tag, Option } from 'core/types'
+import useInventory from 'core/hooks/use-inventory'
 
 interface OptionsMap {
-    [tag: string]: string | JSX.Element
+    [tag: Tag]: string | JSX.Element
 }
 interface ResponseProps {
-    tag: string
     options: OptionsMap
+    tag: Tag
 }
+
 const Response = ({ tag, options }: ResponseProps): JSX.Element => {
     // Get the inventory item for this tag
-    const choice = useSelector((state: RootState) => state.inventory.present[tag])
-    if (!choice) {
+    const [option] = useInventory([tag])
+    if (!option) {
         return null
     }
 
     // Perform a case-insensitive match against the user's earlier choice pick
-    const resp = Object.keys(options).filter((t) => {
-        // Assume any plain-string keys are trailing substring queries (e.g. "banana" for "a ripe banana")
-        //
-        // Use a language-agnostic regexp here; \W is insufficient
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
-        // eslint-disable-next-line no-control-regex
-        if (/([\u0000-\u0019\u0021-\uFFFF])+/gu.test(t)) {
-            t = '*' + t + '*'
-        }
-        return minimatch(choice, t, { nocase: true })
-    })
+    const resp = match(options, option)
 
     if (resp.length === 0) {
         console.group(`Unmatched choice list: "${tag}"`)
         console.log(
             `No matching response was found for tag ${tag} based on option-values ${Object.keys(
                 options
-            )}. Full text of the option selected was: `
+            )}`
         )
 
         console.groupEnd()
@@ -44,12 +35,51 @@ const Response = ({ tag, options }: ResponseProps): JSX.Element => {
     if (resp.length > 1) {
         console.warn('More than one choice matched the pattern; using the first match.')
     }
-
-    const output = options[resp[0]]
+    return returnMatch(options, resp[0])
+}
+const returnMatch = (options: OptionsMap, response: string): JSX.Element => {
+    const output = options[response]
 
     if (typeof output === 'string') {
         return <>{output}</>
     }
     return output
+}
+
+const match = (options: OptionsMap, option: Option) => {
+    return Object.keys(options).filter((t) => {
+        // Assume any plain-string keys are trailing substring queries (e.g. "banana" for "a ripe banana")
+        //
+        // Use a language-agnostic regexp here; \W is insufficient
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
+        // eslint-disable-next-line no-control-regex
+        if (/([\u0000-\u0019\u0021-\uFFFF])+/gu.test(t)) {
+            t = '*' + t + '*'
+        }
+        return minimatch(option, t, { nocase: true })
+    })
+}
+interface MultiResponseProps {
+    tags: Tag[]
+    options: OptionsMap
+}
+/**
+ * A response component that evaluates the same options list against all passed tags
+ * and returns the response in the matching option map _only_ if all tags matched.
+ * This is most useful when passing a wildcard like '*' to only return a response if
+ * all tags are populated. This is similar to using <When> except you don't need to retrieve
+ * the inventory values in the containing component, and useful if you want to avoid unnecessary
+ * re-renders to retrieve it.
+ *
+ * @param param0
+ * @returns
+ */
+export const MultiResponse = ({ tags, options }: MultiResponseProps): JSX.Element => {
+    const inventory = useInventory(tags)
+    const resp = tags
+        .filter((t, i) => inventory[i] != null)
+        .map((t) => match(options, t))
+        .filter((r) => r.length > 0)
+    return resp.length === tags.length ? returnMatch(options, resp[0][0]) : null
 }
 export default Response
--- stories/playground/chapters/game-of-cards.tsx
+++ stories/playground/chapters/game-of-cards.tsx
@@ -2,13 +2,16 @@ import * as React from 'react'
 
 import { Transition, useTransition, animated, SpringConfig, config, to } from '@react-spring/web'
 
-import { C } from 'core/components'
-import { PageType } from 'core/types'
+import { C, R, When } from 'core/components'
+import { Next, Option, PageType, Tag } from 'core/types'
 
 import cards from 'public/stories/playground/styles/Cards.module.scss'
 
 import { ChapterContext, useChapterSetup } from 'core/components/chapter'
 import useChapter from 'core/hooks/use-chapter'
+import ImageChoice from 'core/components/widgets/image-choice'
+import useInventory from 'core/hooks/use-inventory'
+import { MultiResponse } from 'core/components/response'
 
 const filename = 'game-of-cards'
 const scenes = 999
@@ -19,11 +22,16 @@ const Stage: React.FC = ({ children }) => {
 type SceneContextProps = {
     show: boolean
 }
+interface Chooseable {
+    tag: Tag
+    alt: string
+}
 interface Item {
     cls: string[]
     x: number
     y: number
     z: number
+    c?: Chooseable
 }
 interface SceneryProps {
     items: Item[]
@@ -52,8 +60,23 @@ const Scenery = ({
                         transform: to([x, y, z], (x, y, z) => `translate3d(${x}px, ${y}px, ${z}px)`)
                     }}
                     className={item.cls
-                        .map((c) => `${className} ${cards[c]}`)
-                        .join(' ')}></animated.div>
+                        .map((c) => `${className} ${cards[c]} ${item.c ? cards.chooseable : ''}`)
+                        .join(' ')}>
+                    {item.c && (
+                        <C
+                            tag={item.c.tag}
+                            options={[[item.c.tag as Option]]}
+                            widget={ImageChoice}
+                            extra={{
+                                src: `/stories/playground/images/${item.c.tag}.png`,
+                                alt: item.c.alt,
+                                option: item.c.tag as Option
+                            }}
+                            persist={true}
+                            next={Next.None}
+                        />
+                    )}
+                </animated.div>
             )
     )
 }
@@ -111,23 +134,50 @@ export const Page: PageType = () => {
                                 { cls: ['sea'], x: 0, y: -100, z: 0 },
                                 { cls: ['lighthouse'], x: 200, y: 0, z: 0 }
                             ]}
-                        />{' '}
-                        {/* Sea */}
-                        <Scenery
-                            sceneConfig={config.stiff}
-                            className={cards.object}
-                            items={[
-                                { cls: ['sea'], x: 0, y: -100, z: 0 },
-                                { cls: ['lighthouse'], x: 200, y: 0, z: 0 }
-                            ]}
                         />
                         <Card className={cards.card1}>
-                            <h2>Talk to the animals!</h2>
-                            <p>
-                                It's a beautiful day at the beach and you can talk to animals. What
-                                do they have to say?
-                            </p>
-                            {/* <C options={[['Go to next scene']]} tag="scene1" persist={true} /> */}
+                            <p>It's a beautiful day at the beach and you can talk to animals.</p>
+                            <div className={cards.responses}>
+                                <div>
+                                    <R
+                                        tag="crab1"
+                                        options={{ '*': <p>"Hello!," says the hermit crab.</p> }}
+                                    />
+                                </div>
+                                <div>
+                                    <R
+                                        tag="crab2"
+                                        options={{
+                                            '*': <p>"Salutations," says the hermit crab.</p>
+                                        }}
+                                    />
+                                </div>
+                                <div>
+                                    <R
+                                        tag="gull"
+                                        options={{
+                                            '*': (
+                                                <p>
+                                                    "There's no ethical consumption under
+                                                    capitalism," says the gull.
+                                                </p>
+                                            )
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <MultiResponse
+                                tags={['crab1', 'crab2', 'gull']}
+                                options={{
+                                    '*': (
+                                        <C
+                                            options={[['Visit more animals!']]}
+                                            tag="scene1"
+                                            persist={true}
+                                        />
+                                    )
+                                }}
+                            />
                         </Card>
                         {/* Beach items */}
                         <Scenery
@@ -143,9 +193,42 @@ export const Page: PageType = () => {
                             className={cards.object}
                             sceneConfig={config.molasses}
                             items={[
-                                { cls: ['crab1'], x: -200, y: 0, z: 0 },
-                                { cls: ['crab2'], x: -1000, y: 0, z: 0 },
-                                { cls: ['gull'], x: 0, y: -100, z: 0 }
+                                {
+                                    cls: ['crab1'],
+                                    c: {
+                                        alt: 'A hermit crab',
+                                        tag: 'crab1'
+                                    },
+                                    x: -200,
+                                    y: 0,
+                                    z: 0
+                                },
+                                {
+                                    cls: ['crab2'],
+                                    c: {
+                                        alt: 'A hermit crab',
+                                        tag: 'crab2'
+                                    },
+                                    x: -1000,
+                                    y: 0,
+                                    z: 0
+                                }
+                            ]}
+                        />
+                        <Scenery
+                            className={cards.object}
+                            sceneConfig={{ tension: 180, friction: 6 }}
+                            items={[
+                                {
+                                    cls: ['gull'],
+                                    c: {
+                                        alt: 'A gull',
+                                        tag: 'gull'
+                                    },
+                                    x: 0,
+                                    y: -100,
+                                    z: 0
+                                }
                             ]}
                         />
                     </Scene>
