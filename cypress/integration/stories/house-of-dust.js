79a7aa0fc35babd3a2c9d6be273c315065fa236e
Liza Daly <lizadaly@gmail.com>
2021-11-25 19:57:26 -0500

    Remove tests

--- cypress/integration/stories/house-of-dust.js
+++
@@ -1,7 +0,0 @@
-describe('Full test of the Time Machine story', () => {
-    it('Checks that the story works', () => {
-        cy.visit('/playground')
-        cy.get('a:contains("The House of Dust")').click()
-        cy.contains('A house of').should('exist')
-    })
-})
