9b99cd5c893879a054c33429c129da79c3e1a135
Liza Daly <lizadaly@gmail.com>
2021-12-06 14:12:00 -0500

    Add teleport feature

--- core/multiplayer/components/debug.tsx
+++ core/multiplayer/components/debug.tsx
@@ -2,15 +2,24 @@ import * as React from 'react'
 
 import useChapter from 'core/hooks/use-chapter'
 import { PlayerContext } from 'core/multiplayer/components/multiplayer-init'
+import { v4 as uuidv4 } from 'uuid'
 
 import debug from 'public/styles/multiplayer/Debug.module.scss'
 import { StoryContext } from 'pages/[story]/[[...chapter]]'
 import { useDispatch, useSelector } from 'react-redux'
 import { Config, RootState, Toc } from 'core/types'
-import { emitNavChange, emitPresence, pollForPresence } from '../api-client'
+import {
+    emitChoice,
+    emitNavChange,
+    emitPresence,
+    getStoryUrl,
+    pollForPresence,
+    resumeStoryInstance
+} from '../api-client'
 import { Player } from '.prisma/client'
 import { Multiplayer } from 'core/features/multiplayer'
 import { PresenceApiResponse } from 'pages/api/core/story/[story]/[instance]/presence'
+import { Persistor } from 'redux-persist'
 
 /**
  * Display debugging info and allow for specific events to be triggered via the API.
@@ -18,16 +27,45 @@ import { PresenceApiResponse } from 'pages/api/core/story/[story]/[instance]/pre
 const Debug = (): JSX.Element => {
     const context = React.useContext(PlayerContext).presenceApiResponse
     const multiplayer = useSelector((state: RootState) => state.multiplayer.multiplayer)
-    const { config } = React.useContext(StoryContext)
-    console.log('Debug')
+    const { config, persistor } = React.useContext(StoryContext)
     return (
         <div className={debug.content}>
-            <h3>Debug toolbar</h3>
             <LocationSwitcher config={config} multiplayer={multiplayer} context={context} />
+            <UserSwitcher config={config} multiplayer={multiplayer} persistor={persistor} />
         </div>
     )
 }
 
+interface UserSwitcherProps {
+    config: Config
+    multiplayer: Multiplayer
+    persistor: Persistor
+}
+
+const UserSwitcher = ({ config, multiplayer, persistor }: UserSwitcherProps): JSX.Element => {
+    return (
+        <div>
+            Switch user to{' '}
+            <button
+                onClick={() => {
+                    persistor.flush().then(() => {
+                        persistor.pause()
+                        persistor.purge().then(() => {
+                            localStorage.clear()
+                            persistor.persist()
+                            location.assign(
+                                getStoryUrl(multiplayer.instanceId) +
+                                    '&playerId=' +
+                                    multiplayer.otherPlayer.id
+                            )
+                        })
+                    })
+                }}>
+                {multiplayer.otherPlayer.name}
+            </button>
+        </div>
+    )
+}
 interface LocationSwitcherProps {
     config: Config
     multiplayer: Multiplayer
@@ -49,7 +87,7 @@ const LocationSwitcher = ({ config, multiplayer, context }: LocationSwitcherProp
                         <RelocateButton
                             identifier={config.identifier}
                             instanceId={multiplayer.instanceId}
-                            otherPlayer={multiplayer.otherPlayer}
+                            player={multiplayer.otherPlayer}
                         />
                     </div>
                 </>
@@ -59,7 +97,7 @@ const LocationSwitcher = ({ config, multiplayer, context }: LocationSwitcherProp
                     <MoveButton
                         identifier={config.identifier}
                         instanceId={multiplayer.instanceId}
-                        otherPlayer={multiplayer.otherPlayer}
+                        player={multiplayer.otherPlayer}
                         location={start}
                     />
                 </>
@@ -70,23 +108,19 @@ const LocationSwitcher = ({ config, multiplayer, context }: LocationSwitcherProp
 interface RelocateButtonProps {
     identifier: string
     instanceId: string
-    otherPlayer: Player
+    player: Player
 }
-const RelocateButton = ({
-    identifier,
-    instanceId,
-    otherPlayer
-}: RelocateButtonProps): JSX.Element => {
+const RelocateButton = ({ identifier, instanceId, player }: RelocateButtonProps): JSX.Element => {
     const { toc } = useSelector((state: RootState) => state.navigation.present)
     return (
         <>
-            Move to location:
+            Move {player.name} to location:
             {Object.values(toc).map((t) => (
                 <MoveButton
                     key={t.filename}
                     identifier={identifier}
                     instanceId={instanceId}
-                    otherPlayer={otherPlayer}
+                    player={player}
                     location={t.filename}
                 />
             ))}
@@ -97,19 +131,21 @@ const RelocateButton = ({
 interface MoveButtonProps {
     identifier: string
     instanceId: string
-    otherPlayer: Player
+    player: Player
     location: string
 }
-const MoveButton = ({ identifier, instanceId, otherPlayer, location }: MoveButtonProps) => {
+const MoveButton = ({ identifier, instanceId, player, location }: MoveButtonProps) => {
     const [posting, setPosting] = React.useState(false)
+    React.useEffect(() => {
+        const timeout = setTimeout(() => setPosting(false), 5000)
+        return () => clearTimeout(timeout)
+    })
 
     return (
         <button
             onClick={() => {
-                console.log(
-                    `Will post ${identifier} ${instanceId} ${otherPlayer.name} to ${location}`
-                )
-                emitNavChange(identifier, location, instanceId, otherPlayer.id)
+                console.log(`Will post ${identifier} ${instanceId} ${player.name} to ${location}`)
+                emitNavChange(identifier, location, instanceId, player.id)
                 setPosting(true)
             }}>
             {posting ? 'posting...' : location}
