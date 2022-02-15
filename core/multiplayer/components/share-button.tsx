8c3bce8dafdf0769f83ccf656392e4c4b6afca60
Liza Daly <lizadaly@gmail.com>
2022-02-15 13:30:22 -0500

    More clear CTA for share button

--- core/multiplayer/components/share-button.tsx
+++ core/multiplayer/components/share-button.tsx
@@ -4,17 +4,25 @@ import Image from 'next/image'
 
 import styles from 'public/styles/multiplayer/ShareButton.module.scss'
 import { MultiplayerContext } from 'core/multiplayer/components/multiplayer'
+import { usePresence } from '../hooks/use-presence'
 
 const ShareButton = (): JSX.Element => {
     const { storyUrl, otherPlayer } = React.useContext(MultiplayerContext).multiplayer
+    const { isActive } = usePresence()
+    const [clicked, isClicked] = React.useState(false)
+
     return (
         <>
             <button
-                className={styles.clipboard}
-                onClick={() =>
+                className={`${styles.clipboard} ${isActive ? '' : styles.offline}`}
+                onClick={() => {
                     navigator.clipboard.writeText(storyUrl + `&playerId=${otherPlayer.id}`)
-                }>
-                <span>Share story link with {otherPlayer.name}</span>
+                    isClicked(true)
+                    setTimeout(() => isClicked(false), 5000)
+                }}>
+                <span>
+                    {clicked ? 'Copied to clipboard!' : 'Share story link with the other player'}{' '}
+                </span>
                 <Image src="/images/clipboard.svg" width={25} height={25} alt="Copy to clipboard" />
             </button>
         </>
