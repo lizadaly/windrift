f63a1608a59aeb147fd23c766ef29e5370bd1b4d
Liza Daly <lizadaly@gmail.com>
2022-07-05 20:13:27 -0400

    Use effect to delay loading of the dynamic components

--- core/components/story.tsx
+++ core/components/story.tsx
@@ -29,8 +29,13 @@ interface StoryProps {
 
 const Story = ({ story }: StoryProps): JSX.Element => {
     const toc = useSelector((state: RootState) => state.navigation.present.toc)
-    const [components] = React.useState(() => chapterComponents(toc, story))
-    return (
+    const [components, setComponents] = React.useState<ChapterComponent[]>(null)
+
+    React.useEffect(() => {
+        setComponents(chapterComponents(toc, story))
+    }, [])
+
+    return components ? (
         <>
             {Object.values(toc)
                 .filter((c) => c.visible)
@@ -44,6 +49,6 @@ const Story = ({ story }: StoryProps): JSX.Element => {
                     </div>
                 ))}
         </>
-    )
+    ) : null
 }
 export default Story
