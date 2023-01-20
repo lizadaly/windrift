8197e4c850209ac2553b66ce0c792470802fd507
Liza Daly <lizadaly@gmail.com>
2023-01-20 14:23:18 -0500

    Upgrade to Next 13

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withMDX({
     trailingSlash: true,
-    assetPrefix: isProd ? '../' : '',
+    assetPrefix: isProd ? '../' : undefined,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
 })
