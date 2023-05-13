0a5a4e2572e8e45a4bc908e994896645d7e0ea66
6notes <jlu6.proton@proton.me>
2023-05-13 16:08:10 -0700

    Update test directory in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-    /* Look for test files in the "tests" directory, relative to this configuration file. */
-    testDir: 'tests',
-
-    /* Run all tests in parallel. */
+    testDir: './playwright/e2e/core',
+    /* Run tests in files in parallel */
     fullyParallel: true,
 
     /* Fail the build on CI if you accidentally left test.only in the source code. */
