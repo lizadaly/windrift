8115bf3423667ca56097ff86b2814b6c815aad8f
6notes <jlu6.proton@proton.me>
2023-05-13 16:21:16 -0700

    Remove commented Cypress code

--- playwright/e2e/core/browser-nav.spec.ts
+++ playwright/e2e/core/browser-nav.spec.ts
@@ -3,21 +3,13 @@ import { expect, test } from '@playwright/test'
 test.describe('Full test of browser forward/back features', () => {
     test('Checks back', async ({ page }) => {
         await page.goto('/manual')
-        //cy.visit('/manual')
         await page.getByRole('link').filter({ hasText: 'Start learning' }).click()
-        //cy.get("a:contains('Start learning')").click()
         await page.getByRole('link').filter({ hasText: 'about choices' }).click()
-        //cy.get('a:contains("about choices")').click()
         await expect(page.getByRole('link').filter({ hasText: 'ripe banana' })).toBeVisible()
-        //cy.get('a:contains("ripe banana")').should('exist')
         await page.getByRole('link').filter({ hasText: 'bulbous orange' }).click()
-        //cy.get('a:contains("bulbous orange")').click()
         await page.goBack()
-        //cy.go('back')
         await expect(page.getByRole('link').filter({ hasText: 'bulbous orange' })).toBeVisible()
-        //cy.get('a:contains("bulbous orange")').should('exist')
         // Now make a different choice
         await page.getByRole('link').filter({ hasText: 'ripe banana' }).click()
-        //cy.get('a:contains("ripe banana")').click()
     })
 })
