4f810c15b2dd0f4797068027e4d06f6adf8f3872
6notes <jlu6.proton@proton.me>
2023-05-13 16:16:24 -0700

    Convert page visiting

--- playwright/e2e/core/browser-nav.spec.ts
+++ playwright/e2e/core/browser-nav.spec.ts
@@ -1,8 +1,9 @@
 import { test } from '@playwright/test'
 
 test.describe('Full test of browser forward/back features', () => {
-    test('Checks back', () => {
-        cy.visit('/manual')
+    test('Checks back', async ({ page }) => {
+        await page.goto('/manual')
+        //cy.visit('/manual')
         cy.get("a:contains('Start learning')").click()
         cy.get('a:contains("about choices")').click()
         cy.get('a:contains("ripe banana")').should('exist')
