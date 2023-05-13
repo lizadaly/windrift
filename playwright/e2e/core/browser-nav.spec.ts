7fcdf27989fbcf826319e8ebf414599e84f7cd36
6notes <jlu6.proton@proton.me>
2023-05-13 16:20:18 -0700

    Convert browser back command

--- playwright/e2e/core/browser-nav.spec.ts
+++ playwright/e2e/core/browser-nav.spec.ts
@@ -12,7 +12,8 @@ test.describe('Full test of browser forward/back features', () => {
         //cy.get('a:contains("ripe banana")').should('exist')
         await page.getByRole('link').filter({ hasText: 'bulbous orange' }).click()
         //cy.get('a:contains("bulbous orange")').click()
-        cy.go('back')
+        await page.goBack()
+        //cy.go('back')
         await expect(page.getByRole('link').filter({ hasText: 'bulbous orange' })).toBeVisible()
         //cy.get('a:contains("bulbous orange")').should('exist')
         // Now make a different choice
