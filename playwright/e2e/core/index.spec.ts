b434d7709fda3507811e62dabf4ab33ca2bf79ec
6notes <jlu6.proton@proton.me>
2023-05-13 23:44:02 -0700

    Convert page visit

--- playwright/e2e/core/index.spec.ts
+++ playwright/e2e/core/index.spec.ts
@@ -1,8 +1,10 @@
 import { test } from '@playwright/test'
 
 test.describe('Check the main index page', () => {
-    test('There should be an active default index page that lists the installed stories', () => {
-        cy.visit('/')
+    test('There should be an active default index page that lists the installed stories', async ({
+        page
+    }) => {
+        await page.goto('/')
         cy.get("a:contains('stone-harbor-pt')").click()
     })
 })
