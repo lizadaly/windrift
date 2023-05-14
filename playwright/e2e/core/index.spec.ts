e5be63334b35eee062e13aa808714b15ec21c7d1
6notes <jlu6.proton@proton.me>
2023-05-13 23:43:16 -0700

    Use Playwright tests

--- playwright/e2e/core/index.spec.ts
+++ playwright/e2e/core/index.spec.ts
@@ -1,5 +1,7 @@
-describe('Check the main index page', () => {
-    it('There should be an active default index page that lists the installed stories', () => {
+import { test } from '@playwright/test'
+
+test.describe('Check the main index page', () => {
+    test('There should be an active default index page that lists the installed stories', () => {
         cy.visit('/')
         cy.get("a:contains('stone-harbor-pt')").click()
     })
