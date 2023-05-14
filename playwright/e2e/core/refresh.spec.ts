3597f93b076e520ddcf5b8d2d8bf93f359ac060a
6notes <jlu6.proton@proton.me>
2023-05-13 23:56:34 -0700

    Convert page reloads

--- playwright/e2e/core/refresh.spec.ts
+++ playwright/e2e/core/refresh.spec.ts
@@ -8,7 +8,7 @@ test.describe('Full test of refresh/rehydration features', () => {
         await page.getByRole('link').filter({ hasText: 'about choices' }).click()
         cy.get('a:contains("ripe banana")')
         await page.getByRole('link').filter({ hasText: 'bulbous orange' }).click()
-        cy.reload()
+        await page.reload()
         cy.get('a:contains("ripe banana")').should('not.exist')
         await page.getByRole('link').filter({ hasText: 'fine choices' }).click()
         await page.getByRole('link').filter({ hasText: 'orange kale' }).click()
@@ -18,7 +18,7 @@ test.describe('Full test of refresh/rehydration features', () => {
 
         await page.getByRole('link').filter({ hasText: 'elm' }).click()
         cy.contains('elm (selected)')
-        cy.reload()
+        await page.reload()
         cy.contains('elm (selected)')
     })
 })
